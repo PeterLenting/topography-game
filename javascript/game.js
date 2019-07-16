29c4e4d4ffb8c5f9b72dc3740e1791e01af9f283
Ubuntu <ubuntu@ip-172-31-25-73.ec2.internal>
2019-07-16 08:57:59 +0000

    upload for tutor-question

--- javascript/game.js
+++ javascript/game.js
@@ -1,69 +1,57 @@
-//https://gist.github.com/stephenscaff/8266351  
-//Add your images
-    
-    
-//Build the img, then do a bit of maths to randomize load and append to a div.
-   //$('<img class="fade-in" src="images/' + imagesArray[Math.floor(Math.random() * imagesArray.length)] + '">').appendTo('#banner-load');
-var imagesArray = ['cyclist-1.png', 'cyclist-2.png', 'cyclist-3.png', 'cyclist-4.png', 'cyclist-5.png', 'cyclist-6.png', 'cyclist-7.png', 'cyclist-8.png'];
-var shownImages = []
+/*let images = ['images/cyclist-1-empty.png', 'images/cyclist-2-empty.png', 'images/cyclist-3-empty.png', 'images/cyclist-4-empty.png', 'images/cyclist-5-empty.png', 'images/cyclist-6-empty.png', 'images/cyclist-7-empty.png', 'images/cyclist-8-empty.png']; */
 
-function displayImage(){
-var num = Math.floor(Math.random() * (imagesArray.length));
-document.canvas.src = imagesArray[num];
-}
 
-[
-	{
-		subjectTitle: "Cycling",
-		data: [
-			{
-				answer: "Jacques Anquetil",
-				question: "images/cyclist-1-empty.png"
-			},
-			{
-				answer: "Lance Armstrong",
-				question: "images/cyclist-2-empty.png"
-			},
-			{
-				answer: "Gino Bartali",
-				question: "images/cyclist-3-empty.png"
-			},
-			{
-				answer: "Fausto Coppi",
-				question: "images/cyclist-4-empty.png"
-			},
-				{
-				answer: "Miguel Indurain",
-				question: "images/cyclist-5-empty.png"
-			},
-			{
-				answer: "Bernard Hinault",
-				question: "images/cyclist-6-empty.png"
-			},
-			{
-				answer: "Louison Bobet",
-				question: "images/cyclist-7-empty.png"
-			},
-			{
-				answer: "Felice Gimondi",
-				question: "images/cyclist-8-empty.png"
-			},
-				{
-				answer: "Eddy Merckx",
-				question: "images/cyclist-9-empty.png"
-			},
-			{
-				answer: "Sean Kelly",
-				question: "images/cyclist-10-empty.png"
-			},
-			{
-				answer: "Alfredo Binda",
-				question: "images/cyclist-11-empty.png"
-			},
-			{
-				answer: "Rik van Steenbergen",
-				question: "images/cyclist-12-empty.png"
-			}
-		]
-	}
-];
+jQuery(document).ready(function($){
+
+    $("#buttonStart").click(function(){
+        $("#buttonStart").hide();
+        let images = ['images/cyclist-1-empty.png', 'images/cyclist-2-empty.png', 'images/cyclist-3-empty.png', 'images/cyclist-4-empty.png', 'images/cyclist-5-empty.png', 'images/cyclist-6-empty.png', 'images/cyclist-7-empty.png', 'images/cyclist-8-empty.png']; 
+        $("#newImage").attr("src", images[Math.floor(Math.random() * images.length -1)]);    
+        $("#buttonHint").show("slow");
+        $("#buttonNewImage").show();
+        $("#textField").show();
+    });
+
+
+
+ /*     let images = ['images/cyclist-1-empty.png', 'images/cyclist-2-empty.png', 'images/cyclist-3-empty.png', 'images/cyclist-4-empty.png', 'images/cyclist-5-empty.png', 'images/cyclist-6-empty.png', 'images/cyclist-7-empty.png', 'images/cyclist-8-empty.png']; 
+      $("#buttonNewImage").on({
+           'click': function(){
+               $("#newImage").attr("src", images[Math.floor(Math.random() * images.length -1)]);
+           }
+       });
+ */
+
+            $("#buttonHint").on({
+                  'click': function(){
+            $("#newImage").each(function() {
+                  var nSrc= $(this).attr('src').replace("-empty.png", "-flag.png");
+                  $(this).attr('src', nSrc);
+              })
+            }});
+        });
+
+
+        let images = ['images/cyclist-1-empty.png', 'images/cyclist-2-empty.png', 'images/cyclist-3-empty.png', 'images/cyclist-4-empty.png', 'images/cyclist-5-empty.png', 'images/cyclist-6-empty.png', 'images/cyclist-7-empty.png', 'images/cyclist-8-empty.png'];
+
+        var usedImages = {};
+        var usedImagesCount = 0;
+        
+        function displayImage(){
+        
+            var num = Math.floor(Math.random() * (images.length));
+            if (!usedImages[num]){
+                document.getElementById("#newImage").getAttribute("src") = images[num];
+                usedImages[num] = true;
+                usedImagesCount++;
+                if (usedImagesCount === images.length){
+                    usedImagesCount = 0;
+                    usedImages = {};
+                }
+            } else {
+                displayImage();
+            }
+        }
+
+
+
