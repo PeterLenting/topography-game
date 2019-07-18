4c9599310cda94a64eb517cf2262022fd6b1c44e
Ubuntu <ubuntu@ip-172-31-25-73.ec2.internal>
2019-07-18 17:09:48 +0000

    Added looping through image-array without repeat

--- javascript/game.js
+++ javascript/game.js
@@ -1,57 +1,77 @@
-/*let images = ['images/cyclist-1-empty.png', 'images/cyclist-2-empty.png', 'images/cyclist-3-empty.png', 'images/cyclist-4-empty.png', 'images/cyclist-5-empty.png', 'images/cyclist-6-empty.png', 'images/cyclist-7-empty.png', 'images/cyclist-8-empty.png']; */
+let images = ['images/cyclist-1-empty.png', 'images/cyclist-2-empty.png', 'images/cyclist-3-empty.png', 'images/cyclist-4-empty.png', 'images/cyclist-5-empty.png', 'images/cyclist-6-empty.png', 'images/cyclist-7-empty.png', 'images/cyclist-8-empty.png', 'images/cyclist-9-empty.png', 'images/cyclist-10-empty.png', 'images/cyclist-11-empty.png', 'images/cyclist-12-empty.png', 'images/cyclist-13-empty.png', 'images/cyclist-14-empty.png'];
 
+var usedImages = {};
+var usedImagesCount = 0;
+
+function displayImage(){
+
+    var num = Math.floor(Math.random() *10);
+    if (!usedImages[num]){
+        document.getElementById("newImage").src = images[num];
+        usedImages[num] = true;
+        usedImagesCount++;
+        if (usedImagesCount === images.length){
+            usedImagesCount = 0;
+            usedImages = {};
+        }
+    } else {
+        displayImage();
+    }
+}
 
 jQuery(document).ready(function($){
 
     $("#buttonStart").click(function(){
         $("#buttonStart").hide();
-        let images = ['images/cyclist-1-empty.png', 'images/cyclist-2-empty.png', 'images/cyclist-3-empty.png', 'images/cyclist-4-empty.png', 'images/cyclist-5-empty.png', 'images/cyclist-6-empty.png', 'images/cyclist-7-empty.png', 'images/cyclist-8-empty.png']; 
-        $("#newImage").attr("src", images[Math.floor(Math.random() * images.length -1)]);    
-        $("#buttonHint").show("slow");
+        let images = ['images/cyclist-1-empty.png', 'images/cyclist-2-empty.png', 'images/cyclist-3-empty.png', 'images/cyclist-4-empty.png', 'images/cyclist-5-empty.png', 'images/cyclist-6-empty.png', 'images/cyclist-7-empty.png', 'images/cyclist-8-empty.png', 'images/cyclist-9-empty.png', 'images/cyclist-10-empty.png', 'images/cyclist-11-empty.png', 'images/cyclist-12-empty.png', 'images/cyclist-13-empty.png', 'images/cyclist-14-empty.png'];
+
+var usedImages = {};
+var usedImagesCount = 0;
+
+function displayImage(){
+
+    var num = Math.floor(Math.random() *10);
+    if (!usedImages[num]){
+        document.getElementById("newImage").src = images[num];
+        usedImages[num] = true;
+        usedImagesCount++;
+        if (usedImagesCount === images.length){
+            usedImagesCount = 0;
+            usedImages = {};
+        }
+    } else {
+        displayImage();
+    }
+} 
+        $("#buttonHint").show();
         $("#buttonNewImage").show();
         $("#textField").show();
     });
 
 
-
- /*     let images = ['images/cyclist-1-empty.png', 'images/cyclist-2-empty.png', 'images/cyclist-3-empty.png', 'images/cyclist-4-empty.png', 'images/cyclist-5-empty.png', 'images/cyclist-6-empty.png', 'images/cyclist-7-empty.png', 'images/cyclist-8-empty.png']; 
-      $("#buttonNewImage").on({
-           'click': function(){
-               $("#newImage").attr("src", images[Math.floor(Math.random() * images.length -1)]);
-           }
-       });
- */
-
-            $("#buttonHint").on({
-                  'click': function(){
+            $("#buttonHint").click(function(){
             $("#newImage").each(function() {
                   var nSrc= $(this).attr('src').replace("-empty.png", "-flag.png");
                   $(this).attr('src', nSrc);
               })
-            }});
-        });
-
-
-        let images = ['images/cyclist-1-empty.png', 'images/cyclist-2-empty.png', 'images/cyclist-3-empty.png', 'images/cyclist-4-empty.png', 'images/cyclist-5-empty.png', 'images/cyclist-6-empty.png', 'images/cyclist-7-empty.png', 'images/cyclist-8-empty.png'];
-
-        var usedImages = {};
-        var usedImagesCount = 0;
+            });
         
-        function displayImage(){
-        
-            var num = Math.floor(Math.random() * (images.length));
-            if (!usedImages[num]){
-                document.getElementById("#newImage").getAttribute("src") = images[num];
-                usedImages[num] = true;
-                usedImagesCount++;
-                if (usedImagesCount === images.length){
-                    usedImagesCount = 0;
-                    usedImages = {};
+
+            $("#buttonNewImage2").click(function(){
+                var answer = $("#textField").val(); 
+                var nSrc= $("#newImage").attr('src').replace("-empty.png", ".png");;
+                $("#newImage").attr('src', nSrc);
+                if 
+                (answer.toUpperCase() == "JACQUES ANQUETIL")
+                {
+                document.getElementById("newImage").innerHTML = nSrc;
+            } else if 
+                ($("#newImage").attr('src') == "images/cyclist-2-empty.png" || "images/cyclist-2-flag.png" && answer.toUpperCase == "LANCE ARMSTRONG")
+                {
+                document.getElementById("newImage").innerHTML = "images/cyclist-2.png";   
                 }
-            } else {
-                displayImage();
-            }
-        }
-
-
-
+              else{
+                document.getElementById("newImage").innerHTML = "Wrong";
+                }
+            });
+          });
