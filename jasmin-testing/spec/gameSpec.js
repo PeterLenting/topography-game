7b17f8f3cb2d09f484241f6f7fb5b783370755a6
Ubuntu <ubuntu@ip-172-31-25-73.ec2.internal>
2019-08-22 15:37:16 +0000

    Added giveHint and startGame to jasmine-testing

--- jasmin-testing/spec/gameSpec.js
+++ jasmin-testing/spec/gameSpec.js
@@ -1,8 +1,30 @@
 describe("button function", function() {
     describe("hintButton function", function() {
         it("should replace '-empty.png' with '-flag.png' in html-elements with ID newImage", function() {
-        functionGiveHint()
-        expect($("#newImageBack").attr('src')).toBe("-flag.png");
+            functionGiveHint()
+            expect($("#newImage").attr('src')).toContain("-flag.png");
+        });
+        it("should replace '-empty.png' with '-flag.png' in html-elements with ID newImageBack", function() {
+            functionGiveHint()
+            expect($("#newImage").attr('src')).toContain("-flag.png");
+        });
+    });
+});
+
+describe("button function", function() {
+    describe("startGame function", function() {
+        it("should remove class 'hidden' from html-elements with ID textField", function() {
+            functionStartGame();
+            expect($('#textField')).not.toHaveClass('hidden');
+        });
+    });
+});
+
+describe("button function", function() {
+    describe("giveHint function", function() {
+        it("should add class 'gotHint' to html-elements with ID newImage", function() {
+            functionGiveHint();
+            expect($('#newImage')).toHaveClass('gotHint');
         });
     });
 });
